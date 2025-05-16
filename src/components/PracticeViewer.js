d37b013964f8042bc358f95505afc380a6520ddc
SH <tjfgnldkdlel@naver.com>
2025-05-17 03:05:00 +0900

    코드 정리

--- src/components/PracticeViewer.js
+++ src/components/PracticeViewer.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "../styles/PracticeViewer.module.css";
 
 const PracticeViewer = ({ xmlFile }) => {
@@ -6,33 +6,41 @@ const PracticeViewer = ({ xmlFile }) => {
   const apiRef = useRef(null);
 
   useEffect(() => {
-    if (xmlFile && containerRef.current && window.alphaTab?.AlphaTabApi) {
-      const api = new window.alphaTab.AlphaTabApi(containerRef.current, {
+    if (!window.alphaTab || !containerRef.current || !xmlFile) return;
+
+    const settings = {
         file: xmlFile,
-        layoutMode: "horizontal",
-        scrollMode: "horizontal",
-        trackDisplayMode: "ScoreTab",
-        scale: 2.5,
-      });
+        player: {
+            enablePlayer: true,
+            enableCursor: true,
+        },
+        display: {
+            layoutMode: "horizontal",
+            autoScroll: true,
+            drawTitle: true,
+            scale: 1.8,
+        },
+    };
 
-      apiRef.current = api;
+    const api = new window.alphaTab.AlphaTabApi(containerRef.current, settings);
+    apiRef.current = api;
 
-      api.renderFinished.on(() => {
+    api.renderFinished.on(() => {
         const canvas = containerRef.current.querySelector("div");
         if (canvas) {
-          containerRef.current.style.width = `${canvas.scrollWidth}px`;  // 자동으로 맞춰줌
+            containerRef.current.style.width = `${canvas.scrollWidth}px`;  // 자동으로 맞춰줌
         }
-      });
+    });
 
-      return () => {
+    // clean up
+    return () => {
         api?.destroy();       // AlphaTab API 인스턴스 제거 (메모리 누수 방지)
-      };
-    }
+    };
   }, [xmlFile]);
 
   return (
     <div className={styles.viewerContainer} >
-      <div className={styles.viewerCanvas} ref={containerRef} />
+        <div className={styles.viewerCanvas} ref={containerRef} />
     </div>
   );
 };
